7943d4ffdaf6c5011e4d9908f7a6d2bbeab9bdc1
farras55 <farraskuyy@gmail.com>
2024-09-06 10:25:59 +0700

    Praktikum01

---
+++ Week 1/Latihan11.java
@@ -0,0 +1,13 @@
+/*
+ * Latihan11
+ */
+public class Latihan11 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Muhammad Farras Awaludin Alwi");
+        System.out.println("Absen : 11");
+        System.out.println("Kelas : 1D");
+        System.out.println("Prodi : Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
---
+++ Week 1/MyFirstJava11.java
@@ -0,0 +1,5 @@
+public class MyFirstJava11 {
+    public static void main(String[] args) {
+
+    }
+}
