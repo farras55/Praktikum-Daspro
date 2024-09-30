de80ce130deefa50223528ab31d21dc7bba3ea1f
farras55 <farraskuyy@gmail.com>
2024-10-01 00:58:23 +0700

    Praktikum05

---
+++ Praktikum05/PemilihanHariDenganIf.java
@@ -0,0 +1,21 @@
+package Praktikum05;
+
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan angka (1-7): ");
+        int angka = scanner.nextInt();
+
+        if (angka >= 1 && angka <= 5) {
+            System.out.println("Weekday");
+        } else if (angka == 6 || angka == 7) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("Invalid Number");
+        }
+        scanner.close();
+    }
+}
