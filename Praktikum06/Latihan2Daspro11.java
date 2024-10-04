4616397de8feb512bd6eb74fc2fae81544fa1ccd
farras55 <farraskuyy@gmail.com>
2024-10-04 19:19:20 +0700

    Praktikum06

--- Praktikum06/Latihan2Daspro11.java
+++ Praktikum06/Latihan2Daspro11.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Latihan2Daspro11 {
     
     public static void main(String[] args) {
-        // Membuat scanner untuk membaca input dari pengguna
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("...");
@@ -14,27 +13,20 @@ public class Latihan2Daspro11 {
 
 
         if (hari.equalsIgnoreCase("rabu")) {
-            // Meminta input dari pengguna mengenai jenis buku dan jumlah buku
-        System.out.print("Masukkan jenis buku (kamus/novel/lainnya): ");
-        String jenisBuku = scanner.nextLine(); 
+           System.out.print("Masukkan jenis buku (kamus/novel/lainnya): ");
+            String jenisBuku = scanner.nextLine(); 
 
-        // Inisialisasi diskon
         double diskon = 0;
 
-        // Kondisi diskon berdasarkan jenis buku
-        if (jenisBuku.equals("kamus")) {
-            // Diskon awal 10%
+         if (jenisBuku.equals("kamus")) {
             diskon = 10;
             System.out.print("Masukkan jumlah buku yang dibeli: ");
             int jumlahBuku = scanner.nextInt();
 
-
-            // Tambahan diskon jika jumlah buku lebih dari 2
             if (jumlahBuku > 2) {
                 diskon += 2;
             }
         } else if (jenisBuku.equalsIgnoreCase("novel")) {
-            // Diskon awal 7%
             diskon = 7;
             System.out.print("Masukkan jumlah buku yang dibeli: ");
             int jumlahBuku = scanner.nextInt();
@@ -42,7 +34,6 @@ public class Latihan2Daspro11 {
             if (jumlahBuku > 3) {
                 diskon += 2;
             } else {
-                // Tambahan diskon jika jumlah buku <= 3
                 diskon += 1;
             }
         } else {
