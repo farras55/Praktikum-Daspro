5bf3d5565fbd58a7fc353b5cbc009aec68bbfe1a
farras55 <farraskuyy@gmail.com>
2024-10-16 11:25:56 +0700

    Praktikum07

--- Praktikum07/SiakadWhileNoAbsen.java
+++ Praktikum07/SiakadWhileNoAbsen.java
@@ -20,6 +20,7 @@ public class SiakadWhileNoAbsen {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Bagus, pertahankan nillainya!");
             }
             else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");  
