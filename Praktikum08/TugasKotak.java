c32852dd69c0fd5aeb4ee2b79e0c8b751ed09862
farras55 <farraskuyy@gmail.com>
2024-11-01 18:41:33 +0700

    Praktikum08

--- Praktikum08/TugasKotak.java
+++ Praktikum08/TugasKotak.java
@@ -8,7 +8,7 @@ public class TugasKotak {
         Scanner sc = new Scanner(System.in);
 
         
-        System.out.print("Masukkan nilai N (minimal 3): ");
+        System.out.print("Masukkan nilai N : ");
         int N = sc.nextInt();
         sc.close();
 
@@ -16,26 +16,14 @@ public class TugasKotak {
             System.out.println("Nilai N harus minimal 3.");
             return;
         }
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
-                    System.out.print(N + " ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-
-        if (N > 9) {
-            System.out.println("Nilai N harus minimal 3.");
+        else if (N >= 10) {
+            System.out.println("Nilai N harus makasimal 9.");
             return;
         }
         
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (i == 0 || i == N - 1 || j == 0 || j == N - 1 || i == 1 || i == N - 1 || j == 1 || j == N - 1) {
+                if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
                     System.out.print(N + " ");
                 } else {
                     System.out.print("  ");
