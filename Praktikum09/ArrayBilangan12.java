f16a2e11153b1400fb393da8e204ea039e13ddcc
farras55 <farraskuyy@gmail.com>
2024-11-08 08:46:10 +0700

    Praktikum09

---
+++ Praktikum09/ArrayBilangan12.java
@@ -0,0 +1,14 @@
+package Praktikum09;
+
+public class ArrayBilangan12 {
+    public static void main(String[] args) {
+        
+        int[] bil = {5, 13, -7, 17};
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+
+    }
+}
---
+++ Praktikum09/MenuMakanan.java
@@ -0,0 +1,12 @@
+package Praktikum09;
+
+import java.util.Scanner;
+
+public class MenuMakanan {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String[] daftarMenu = {"Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Bakar", "Teh Tarik", "Cappucino", "Chocolate Ice"};
+        double[] daftarHarga = {20000, 22000, 15000, 17000, 13000, 18000, 20000};
+    }
+}
